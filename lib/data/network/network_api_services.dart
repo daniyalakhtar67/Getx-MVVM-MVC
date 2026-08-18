4a4fe6f9c5f9eec8e4cb448ff7638067b290e635
DaniyalAkhtar <daniyalakhtarpatoojo125@gmail.com>
2026-08-18 11:59:54 +0500

    Network Services

--- lib/data/network/network_api_services.dart
+++ lib/data/network/network_api_services.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:getx_mvvc/data/app_exceptions.dart';
 import 'package:getx_mvvc/data/network/base_api_services.dart';
 import 'package:http/http.dart' as http;
@@ -9,8 +10,10 @@ class NetworkApiServices extends BaseApiServices{
   Future<dynamic>postApi(dynamic data, String url)async{
     dynamic responseJson;
     try{
-      final response2 = await http.get(Uri.parse(url)).timeout(Duration(seconds: 10));
-      responseJson = returnResponse(response2);
+      final response2 = await http.post(Uri.parse(url),
+        body: jsonEncode(data),
+      );
+
     }on SocketException{
       throw InternetException();
     }on RequestTimeOut{
@@ -21,6 +24,9 @@ class NetworkApiServices extends BaseApiServices{
   }
   @override
   Future<dynamic> getApi(String url)async {
+    // if(kDebugMode){
+    //   print(url);      -> IF we want to print the data
+    // }
     dynamic responseJson;
     try {
       final response = await http.get(Uri.parse(url)).timeout(const Duration(seconds: 10)); // ya time backend developer da ga
